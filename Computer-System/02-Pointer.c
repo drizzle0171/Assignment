8ac17cf923bcd785e3cb67270038b1693970d790
drizzle0171 <yongsandew@gmail.com>
2022-09-23 19:04:19 +0900

    Assignment01

--- Computer-System/02-Pointer.c
+++ Computer-System/02-Pointer.c
@@ -3,36 +3,35 @@
 int main(void){
     char str1[256];
     char str2[128];
-    int len=0; // 기존 문자열의 길이 계산
+    int len=0; // str1의 문자열 길이 계산
 
     char *pt1 = str1; // str1을 가리키는 포인터 변수
     char *pt2 = str2; // str2을 가리키는 포인터 변수
 
+    // 각각의 문자열을 입력 받음
     printf("문자열을 입력하세요: ");
     scanf("%s", str1);
 
     printf("붙일 문자열을 입력하세요: ");
     scanf("%s", str2);
 
-    // 문자열 str1의 길이 계산
-    while (*pt1 != '\0'){
+    // 문자열 str1의 길이 계산 - str1의 문자열 길이를 계산하여 길이만큼 pointer가 뒤를 가리키게 만든 후 str2의 내용 복사
+    while (str1[len] != '\0'){
         len++;
-    }
-    printf("%d\n", len);
+    };
     
     // 문자열을 붙임
     while (*pt2 != '\0') {
-        *(pt1+len) = *pt2;
-        len++;
-        pt2++;
+        *(pt1+len) = *pt2; //pt1은 str1의 가장 처음의 주소를 가리키고 있으므로 pt2는 pt1+len만큼, 즉 pt1의 마지막 부분+1부터 가리켜야 함
+        len++; // len은 str1의 마지막+1만큼의 위치를 알려주는 역할을 하므로 한 칸 씩 뒤로 가야함
+        pt2++; // pt2 역시 다음으로 붙일 문자열을 가리켜야 하므로 ++를 이용해줌
     };
 
-    printf("done");
     // 문자열의 끝을 알리기 위해 문자열의 맨 끝에 NULL 문자 저장
-    // *(pt1+len) = '\0';
+    *(pt1+len) = '\0'; //현재 len은 str1과 str2가 합쳐진 문장의 길이를 의미하므로 pt1의 첫 주소에서 len만큼 뒤로 간 후 NULL 문자 저장
 
     printf("결과: ");
-    printf("%s", str1);
+    printf("%s\n", str1); //결과 출력
 
     return 0;
 }
--- Computer-System/03-Struct.c
+++ Computer-System/03-Struct.c
@@ -5,50 +5,84 @@ typedef struct Node {
       int degree;
       int coefficient;
       struct Node *next;
-      struct Node *prev;
+      struct Node *prev;  
     } Node;
     // Node 구조체는 하나의 타입으로 여겨져야 하므로 typedef 사용
     // Node 구조체의 별명을 Node라고 설정
 
 Node *inputpoly(void){
-    struct Node *first; // 가장 첫 번째 poly를 가리킬 구조체 포인터
-    struct Node *last; // 이전에 입력한 poly를 기억할 구조체 포인터
-    struct Node node; // 입력받는 poly를 저장할 구조체
-    struct Node *pt; // 현재 입력 받는 poly 구조체를 가리킬 구조체 포인터
+    struct Node *first; // 가장 첫 번째 노드를 가리킬 구조체 포인터
+    struct Node *last; // 이전에 입력한 노드를 기억할 구조체 포인터
+
+    struct Node node; // 입력받는 노드를 저장할 구조체 (주석에서 언급하는 노드는 현재 입력 받는 단항을 의미하고, 이 구조체 node는 이 단항을 받는 구조체를 의미합니다.)
+    struct Node *pt; // 현재 입력 받는 node 구조체를 가리킬 구조체 포인터
     pt = &node; // pt가 node를 가리키고 있다는 것을 알려줌
+    
+    struct Node *temp; // 차수 비교를 위한 temp 구조체 포인터
+    struct Node *temp2; // 차수 비교를 위해 필요한 temp를 임시적으로 담아둘 구조체 포인터
 
     first = NULL; // first는 일단 아무도 가리키고 있지 않음
     last = NULL; // last는 일단 아무도 가리키고 있지 않음
+
     while(1) { // 계속해서 입력을 받아야 하므로 무한 루프로 설정
-        float deg, coef; // 현재 입력 받는 두 값을 담아둘 실수 변수 선언 (deg = 차수, coef = 계수)
+        float deg, coef; // 현재 입력 받는 두 값을 담아둘 실수 변수 선언 (deg = 차수, coef = 계수) -> 정수로는 아래에서 int casting
+
         printf("Input (degree) (coefficient): ");
         scanf("%f %f", &deg, &coef); // 변수를 입력 받음
+
         pt = (struct Node*)malloc(1*sizeof(Node)); // node 구조체 포인터 pt에 동적 메모리 할당
         pt -> degree = (int)deg; // pt가 가리키는 구조체 node의 degree에 deg 저장
         pt -> coefficient = (int)coef; // pt가 가리키는 구조체 node의 coefficient에 coef 저장
+
         if (deg < 0 && coef < 0){ //만약 deg와 coef 모두 음수라면 break를 통해 루프 종료
             printf("Done!! \n");
             last -> next = NULL; // 마지막 node의 next라는 구조체 포인터에는 NULL 값 저장
             break;
         }
+
         else if (deg-(int)deg != 0 || coef-(int)coef != 0){ // 실수에서 정수 부분을 뺀 값이 0과 동일하면 정수로 판단 -> deg와 coef가 모두 정수가 아닐 경우 다시 입력하라고 함
             printf("실수를 입력하셨네요. 양의 정수만 입력해주세요. \n(종료하고 싶을 경우 degree와 coefficient를 모두 음의 정수로 입력해주세요.)\n");
         }
-        else if ((deg >= 0) && coef > 0) { // 만약 deg와 coef 모두 양의 정수라면 else if 문 실행
-                if (first == NULL){ // first가 NULL일 때
-                    first = pt; // 지금까지 받은 deg와 coef의 정보가 담긴 구조체를 가리키는 pt를 first와 동일시함
-                    first -> prev = NULL; // 여기서 받은 포인터는 첫 번째 node이므로 prev는 NULL값이 됨
-                }
-                else{ // 두 번째 노드부터 실행될 곳
-                    if (last == NULL){ // last, 즉 마지막 node를 가리키는 last 포인터가 NULL일 때 (여기서는 두 번째 node를 가리킬 포인터가 됨)
-                        first -> next = pt; // first의 next는 현재 받은 pt이므로 이를 저장
-                        pt -> prev = first; // pt, 즉 현재 받은 node의 prev는 first이므로 이를 저장
 
+        else if ((deg >= 0) && coef > 0) { // 만약 deg는 0 이상, coef가 양의 정수라면 else if 문 실행
+                
+                if (first == NULL){ // first가 NULL일 때
+                    first = pt; // 지금까지 받은 deg와 coef의 정보가 담긴 node 구조체를 가리키는 pt를 first에 저장 (first와 pt는 현재 같은 주소값을 가리키고 있음)
+                    first -> prev = NULL; // 여기서 받은 포인터는 첫 번째 node이므로 prev는 NULL값이 됨
+                    temp = first;
+                }
+                
+                else{ // 두 번째 노드부터 실행될 곳
+                    while (1){ // 계산된 전체 노드를 돌면서 차수를 비교할 수 있도록 무한 루프 사용
+                
+                    if (deg == temp->degree){ // 만약 계산된 차수의 합이 node(first라고 가정)의 차수와 같으면
+                        (temp -> coefficient) += coef; // node의 coefficient에 multiply_coef를 더해서 저장
+                        break; // 그럼 해당 노드에 대한 계산은 끝났으니 break로 차수 비교를 위한 무한 루프를 빠져나옴
                     }
-                    else { // 세번째 노드부터 실행
-                        pt -> prev = last; // pt(세번째 노드라고 가정 했을 때)의 이전은 last(last는 지금 두 번째 노드를 가리키고 있음)임
-                        last -> next = pt; // last(두 번째 노드를 가리키고 있음)의 next는 pt(세 번째 노드를 가리키고 있음)임
-                    };
+                
+                    else{ // 만약 계산된 차수의 합이 node(first라고 가정)의 차수와 다를 때
+                
+                        if (temp -> next == NULL){ // node, 즉 차수를 비교하기 위한 node의 next가 NULL(이를 설정하지 않으면 같은 차수의 노드가 또 추가됨)일 때
+                
+                            if (last == NULL){ // last, 즉 마지막 node를 가리키는 last 포인터가 NULL일 때 (여기서는 두 번째 node를 가리킬 포인터가 됨)
+                                first -> next = pt; // first의 next는 현재 받은 result_pt이므로 이를 저장
+                                pt -> prev = first; // result_pt, 즉 현재 받은 node의 prev는 first이므로 이를 저장
+                                last = pt; // 노드 연결 후 다음 번에 받을 노드를 위해 last가 가리키는 곳이 현재의 result_pt가 가리키는 곳으로 바꾸도록 함
+                            }
+                
+                            else { // 세번째 노드부터 실행
+                                pt -> prev = last; // result_pt(세번째 노드라고 가정 했을 때)의 이전은 last(last는 지금 두 번째 노드를 가리키고 있음)임
+                                last -> next = pt; // last(두 번째 노드를 가리키고 있음)의 next는 result_pt(세 번째 노드를 가리키고 있음)임
+                                last = pt;  // 노드 연결 후 다음 번에 받을 노드를 위해 last가 가리키는 곳이 현재의 result_pt가 가리키는 곳으로 바꾸도록 함
+                                last -> prev = temp; // node는 단순히 일시적인 비교를 위해 사용되지만, 최종적으로는 last가 가리키는 곳의 이전이므로 이와 같이 설정
+                            };
+                            
+                            break; // 계산이 완료되면 break로 무한 루프를 빠져나옴
+                        }
+                    }
+                    temp2 = temp; // node가 가리키는 곳을 임시 저장
+                    temp = temp2 -> next; // node의 next가 가리키는 곳을 다시 node에 넣음 (첫 번째 노드와의 차수 비교를 끝냈으면 다음 노드와의 차수 비교를 해야하기 때문)
+                };
                }
             last = pt; // node 연결 후 다음 번에 받을 노드를 위해 last가 가리키는 곳이 현재의 pt가 가리키는 곳으로 바꾸도록 함
         }
@@ -56,13 +90,16 @@ Node *inputpoly(void){
             printf("계수는 양의 정수만 입력해주세요. \n(종료하고 싶을 경우 degree와 coefficient를 모두 음의 정수로 입력해주세요.)\n");
         };
     };
+
     free(pt); // pt에 할당된 동적 메모리를 해제
     pt = NULL; //pt는 다시 NULL 값을 가리키도록 함
     return first; // 첫 번째 노드를 가리키는 first 포인터 반환
 };
 
 void printNode(Node *inp){
+
     struct Node *temp; // inp를 저장할 임시 포인터 선언
+    
     while (inp != NULL){ // inp가 가리키는 곳이 NULL이면 루프 종료
         if (inp->degree == 1){ // inp의 degree가 1, 즉 차수가 1일 때는 (숫자)x로 나오도록 함
             printf("%dx", inp->coefficient);
@@ -79,6 +116,7 @@ void printNode(Node *inp){
         else{ // 그 외의 경우에는 노드들을 +로 연결해줌
             printf(" + ");
         };
+    
         temp = inp; // inp가 가리키는 노드를 temp가 가리키도록 함
         inp = temp -> next; // temp의 next, 즉 inp의 다음 노드 위치를 inp에 저장함
     };
@@ -87,14 +125,18 @@ void printNode(Node *inp){
 Node *multiply(Node *a, Node *b){
     Node *pt1 = a; // a가 가리키는 곳을 가리키는 pt1
     Node *pt2 = b; // b가 가리키는 곳을 가리키는 pt2
+    
     Node *first = NULL; // inputpoly 함수와 동일한 역할
     Node *last = NULL; // inputpoly 함수와 동일한 역할
+    
     Node *temp; // pt1과 pt2가 다음 노드를 가리킬 수 있도록 도와주는 포인터 변수
     Node *temp2; // 차수 비교 시 아용되는 node라는 포인터가 다음 노드를 가리킬 수 있도록 도와주는 포인터 변수
     Node *node = NULL; // 차수 비교 시 이용되는 포인터 변수
+    
     Node result; // 두 항을 곱한 결과를 담을 구조체
     Node *result_pt; // 두 항을 곱한 결과를 담을 구조체를 가리키는 포인터 변수
     result_pt = &result; // result와 result_pt를 연결
+    
     int multiply_coef; // 두 항의 계수를 곱한 값을 저장할 변수
     int sum_deg; // 두 항의 차수를 더한 값을 저장할 변수
 
@@ -103,13 +145,16 @@ Node *multiply(Node *a, Node *b){
             result_pt = (struct Node*)malloc(1*sizeof(Node)); // result_pt에 동적 메모리 할당
             multiply_coef = pt1 -> coefficient * pt2 -> coefficient; // pt1의 coefficient와 pt2의 coefficient의 곱
             sum_deg = pt1 -> degree + pt2 -> degree; // pt1의 degree와 pt2의 degree의 합
+            
             result_pt -> coefficient = multiply_coef; // result_pt가 가리키는 result 구조체의 coefficient에 multiply_coef 저장
             result_pt -> degree = sum_deg; // result_pt가 가리키는 result 구조체의 degree에 sum_deg 저장
+            
             if (first == NULL){ // first가 NULL일 때
                 first = result_pt; // 지금까지 받은 deg와 coef의 정보가 담긴 구조체를 가리키는 result_pt를 first와 동일시함
                 first -> prev = NULL; // 여기서 받은 포인터는 첫 번째 node이므로 prev는 NULL값이 됨
                 node = first; // 차수 비교에 사용되는 node는 first부터 시작될 수 있도록 first를 저장
             }
+            
             else {
                 while (1){ // 계산된 전체 노드를 돌면서 차수를 비교할 수 있도록 무한 루프 사용
                     if (sum_deg == node->degree){ // 만약 계산된 차수의 합이 node(first라고 가정)의 차수와 같으면
