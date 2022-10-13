53887d18d56598330924d150de560613a966d8d7
drizzle0171 <yongsandew@gmail.com>
2022-10-13 23:45:33 +0900

    QBL

---
+++ Java/test/test/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        Solution answer = new Solution();
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+        System.out.println(answer.truck(100, 100, truck_weights));
+}
+
+/*
+2, 10, [7,4,5,6] -> 8
+100, 100, [10] -> 101
+100, 100, [10,10,10,10,10,10,10,10,10,10] -> 110
+ */
+}
+
+
