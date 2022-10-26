ea6801ddc74b34ff213c703bdfb791cd4a0078b9
drizzle0171 <yongsandew@gmail.com>
2022-10-26 23:53:59 +0900

    InOrder

---
+++ Java/test/test/src/BinaryTree.java
@@ -0,0 +1,42 @@
+class Node {
+    int item;
+    Node left, right;
+
+    public Node (int key){
+        item = key;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+    BinaryTree() {
+        root = null;
+    }
+
+    void Inorder (Node node){
+        if (node == null)
+            return;
+        Inorder(node.left);
+        System.out.print(node.item + " ");
+        Inorder(node.right);
+    }
+
+    void Inorder() {
+        Inorder(root);
+    }
+
+    public static void main(String[] args){
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println(
+            "\nInorder traversal of binary tree is "
+        );
+        tree.Inorder();
+    }
+}
